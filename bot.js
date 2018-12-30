36bfbe12b9881a085b85b86e7bc818ece51b0193
lolyadman0012 <41803482+lolyadman0012@users.noreply.github.com>
2018-12-30 06:05:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -88,7 +88,7 @@ client.on('message', async msg => {
                     .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
  
-                    .setFooter("By iiFireKingYTii_#7310")
+                    .setFooter("By €¤GoM¤GoM¤€#1000")
                     msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
                    
                     // eslint-disable-next-line max-depth
